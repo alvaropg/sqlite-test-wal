5302188a0821357e47b0ea06fa5ea3cc5212ec3c
Álvaro Peña <alvaropg@gmail.com>
2022-01-25 10:00:56 +0100

    Random ID and more large text

--- src/sqlite-test-wal.c
+++ src/sqlite-test-wal.c
@@ -1,6 +1,8 @@
 #include <sqlite3.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
    int i;
@@ -54,8 +56,12 @@ int main(int argc, char *argv[])
   /*   sqlite3_free(zErrMsg); */
   /* } */
 
+  srand(time(NULL));
   do {
-    rc = sqlite3_exec(db, "INSERT INTO storage (storage_id, storage_content) VALUES (3, 'Other info content');", callback, 0, &zErrMsg);
+    int storage_id = rand();
+    char query[1500];
+    snprintf(query, 1499, "INSERT INTO storage (storage_id, storage_content) VALUES (%d, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nisl a nisl mattis venenatis vel ac mauris. Nunc ipsum velit, consequat et ex ac, consectetur cursus ex. Nam vel dolor placerat, efficitur nisl a, efficitur urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur lobortis, nunc eget scelerisque efficitur, tortor ligula pulvinar justo, et aliquet eros urna ut neque. Sed pharetra accumsan interdum. Nunc facilisis maximus eros, et consectetur quam ultricies a. Duis eleifend risus id magna tristique lacinia. Integer pharetra enim sollicitudin magna convallis volutpat. Mauris eu dui non quam vehicula lacinia non non massa. Curabitur id ultricies eros. Quisque malesuada venenatis elit rhoncus pretium. Duis lacinia libero velit, ac pellentesque augue facilisis et. Sed at fringilla leo. Donec iaculis risus quam, at efficitur lectus tincidunt pretium.');", storage_id);
+    rc = sqlite3_exec(db, query, callback, 0, &zErrMsg);
     if (rc != SQLITE_OK) {
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
       sqlite3_free(zErrMsg);
